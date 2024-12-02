a96129938e8d68eded8cb68365303dd9719ba313
Luna Null <luna@folf.io>
2024-12-02 03:36:26 +0000

    Refactor title and add navigation to App.vue
    Move title and navigation code from HomeView.vue to App.vue for better code organization. Also, adjust styling to accommodate relocated components and ensure consistent user navigation experience. Update page title from 'VRC Album' to 'VRC Folf' to reflect branding changes.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,64 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+
+const route = useRoute()
+</script>
 
 <template>
+  <div class="title" v-if="$route.name !== 'postsView'">
+    <div class="actions">
+      <font-awesome-icon
+        v-if="$route.name !== 'home'"
+        :icon="['fas', 'home']"
+        @click="$router.push('/home')"
+      />
+    </div>
+    <div>
+      <div class="title-text">Welcome to <strong class="clickable">VRC Folf</strong></div>
+      <div v-if="route.name === 'home'" class="title-body">
+        <p>This site contains all my archived photos from VRChat</p>
+        <p>If you have any issues, my contact details are available at <a href="https://luna.folf.io" target="_blank">https://luna.folf.io/</a></p>
+      </div>
+    </div>
+  </div>
     <Suspense>
         <RouterView />
     </Suspense>
 </template>
+
+<style scoped>
+.title {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 1rem;
+}
+
+.actions {
+  display: flex;
+  align-items: center;
+  justify-items: center;
+
+  color: var(--vt-c-secondary);
+}
+
+.actions * {
+  cursor: pointer;
+}
+
+.title :not(.actions) {
+  text-align: center;
+}
+
+strong {
+  color: var(--vt-c-secondary);
+}
+
+.title-text {
+  font-size: 3rem;
+  line-height: 3.5rem;
+}
+
+.title-body {
+  margin-bottom: 2rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,6 @@
 </script>
 
 <template>
-  <div class="title">
-    <div class="title-text">Welcome to <strong>VRC Folf</strong></div>
-    <div class="title-body">
-      <p>This site contains all my archived photos from VRChat</p>
-      <p>If you have any issues, my contact details are available at <a href="https://luna.folf.io" target="_blank">https://luna.folf.io/</a></p>
-    </div>
-  </div>
-
   <div class="sessions">
     <div class="session-card" @click="$router.push('sessions')">
       <p>Photos grouped sessions</p>
@@ -23,15 +15,6 @@
 </template>
 
 <style scoped>
-.title {
-  text-align: center;
-  margin-bottom: 2rem;
-}
-
-.title-text {
-  font-size: 3rem;
-  line-height: 3.5rem;
-}
 
 .sessions {
   display: grid;
